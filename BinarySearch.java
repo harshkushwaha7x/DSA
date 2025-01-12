04197992bff31cb333ae89f53eeeadb4f5ab773d
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-01-12 23:56:32 +0530

    fifth commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,7 @@
+//Binary Search to find X in sorted array
+
+import java.util.Stack;
+
 class BinarySearch  {
     public int search(int[] nums, int target) {
         int n = nums.length;
@@ -7,6 +11,43 @@ class BinarySearch  {
             if(nums[mid] == target) return mid;
             else if(nums[mid] > target) right = mid - 1;
             else left = mid + 1;
+        }
+        return -1;
+    }
+}
+
+//Implement Lower Bound
+
+class Solution {
+
+    static int findFloor(int[] arr, int k) {
+        // write code here
+        int low = 0;
+        int high = arr.length - 1;
+        if(arr[low]>k){
+            return -1;
+        }
+        if(arr[high]<k){
+            return high;
+        }
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == k){
+                return mid;
+            }else if(arr[mid]>k){
+                if(mid==0){
+                    return -1;
+                }else if(arr[mid - 1]>k){
+                    high = mid-1;
+                }else{
+                    return mid-1;
+                }
+            }
+            else if(arr[mid]>k){
+                high = mid-1;
+            }else{
+                low = mid + 1;
+            }
         }
         return -1;
     }
