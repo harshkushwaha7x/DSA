acf2a681204e8d93fb296e2feeab49b4fc9eff33
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-01-14 18:48:53 +0530

    seventh commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -73,4 +73,20 @@ class Implement_Upper_Bound {
     }
 }
 
+//4 Search Insert Position
 
+class Search_Insert_Position {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid-1;
+            else start = mid+1;
+        }
+
+        return start;
+    }
+}
