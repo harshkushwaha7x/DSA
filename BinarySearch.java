4e132bef27d90bb1c9686d5ca3d8d9746e359988
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-01-21 23:13:02 +0530

    DSA commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -247,3 +247,39 @@ class Solution {
         return ans;
     }
 }
+
+// Search in Rotated Sorted Array ||
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+       int l = 0;
+       int h = nums.length-1;
+       while(l<=h){
+        int mid=l+(h-l)/2;
+        if(nums[mid]==target) return true;
+        if(nums[l]==nums[mid]&& nums[mid]==nums[h]){
+            h--;
+            l++;
+            continue;
+
+        }
+        if(nums[l]<=nums[mid]){
+            if(nums[l]<=target && target<=nums[mid]){
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        else{
+            if(nums[mid]<=target && target<=nums[h]){
+                l = mid+1;
+            }
+            else{
+                h = mid-1;
+            }
+        }
+       }
+       return false;
+    }
+}
