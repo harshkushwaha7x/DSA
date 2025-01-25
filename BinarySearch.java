593ec7ddaa8abba0fc303d2fe5d8136a09c191d2
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-01-25 20:51:51 +0530

    DSA commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -257,3 +257,42 @@ class Solution {
         return count;
     }  
 }
+
+// Single Element in a Sorted Array
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        int si=0,ei=nums.length-1;
+        while(si<=ei){
+            int mid=si+(ei-si)/2;
+            if(mid==0 && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            if(mid==nums.length-1 && nums[mid]!=nums[nums.length-2]){
+                return nums[mid];
+            }
+
+            if(nums[mid-1]!=nums[mid] && nums[mid]!= nums[mid+1]){
+                return nums[mid];
+            }
+
+            if(mid%2==0){
+                if(nums[mid-1]==nums[mid]){
+                    ei=mid-1;
+                }else{
+                    si=mid+1;
+                }
+            } else{
+                if(nums[mid-1]==nums[mid]){
+                    si=mid+1;
+                }else{
+                    ei=mid-1;
+                }
+            }
+        }
+         return -1;
+    }
+}
