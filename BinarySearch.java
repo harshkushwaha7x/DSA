8cd0462abcb345a058c2b69b1ca59c898a75f83a
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-01-26 16:56:48 +0530

    DSA commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -178,3 +178,32 @@ class FindPeakElement {
         return left;
     }
 }
+
+// Square Root
+
+class Solution {
+    int floorSqrt(int n) {
+        // Your code here
+        int low = 1;
+        int high = n/2;
+        int ans = -1;
+        if (n == 1){
+            return 1;
+        }
+        while(low<=high){
+            int mid = low + ((high - low)/2);
+            int num = mid * mid;
+            if (num == n){
+                return mid;
+            }
+            else if (num<n){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
