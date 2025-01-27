d73831af98efbdd4053762ade8d2f8c5f1ccfe93
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-01-27 23:29:20 +0530

    DSA commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -207,3 +207,27 @@ class Solution {
         return ans;
     }
 }
+
+// Find nth root of m
+
+class Solution {
+    public int nthRoot(int n, int m) {
+        // code here
+        int low = 1;
+        int high = m;
+        int ans = 1;
+        while(low<=high){
+            
+            int mid = (low+high)/2;
+            if(Math.pow(mid,n) == m){
+                return mid;
+            }else if(Math.pow(mid,n) < m){
+                low = mid+1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
