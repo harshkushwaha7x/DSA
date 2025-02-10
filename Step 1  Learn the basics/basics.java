c83e70737de715504ef513a82f5f8bd450ead73a
Harsh Kushwaha <115409050+harshkushwaha7x@users.noreply.github.com>
2025-02-10 07:59:47 +0530

    basics.java

--- Step 1  Learn the basics/basics.java
+++ Step 1  Learn the basics/basics.java
@@ -11,12 +11,12 @@ class TrieNode {
     }
 }
 
-public class AutoCompleteSystem {
+public class basics {
     private TrieNode root;
     private TrieNode current;
     private StringBuilder currentQuery;
 
-    public AutoCompleteSystem(String[] sentences, int[] times) {
+    public basics(String[] sentences, int[] times) {
         root = new TrieNode();
         current = root;
         currentQuery = new StringBuilder();
@@ -138,7 +138,7 @@ class Main {
     public static void main(String[] args) {
         String[] sentences = {"hello world", "hello there", "hello java"};
         int[] times = {3, 1, 2};
-        AutoCompleteSystem autoComplete = new AutoCompleteSystem(sentences, times);
+        basics autoComplete = new basics(sentences, times);
 
         System.out.println(autoComplete.input('h'));
         System.out.println(autoComplete.input('e'));
@@ -154,5 +154,264 @@ class Main {
 
         int[] result = PassByValue.passedBy(5, 10);
         System.out.println(Arrays.toString(result));
+    }
+}
+
+// Count Digits
+class CountDigitsSolution {
+    static int evenlyDivides(int n) {
+        int count = 0;
+        int num = n;
+
+        while (n > 0) {
+            int value = n % 10;
+            if (value != 0 && num % value == 0) {
+                count++;
+            }
+            n = n / 10;
+        }
+        return count;
+    }
+}
+
+// Reverse Integer
+class ReverseIntegerSolution {
+    public int reverse(int x) {
+        long sum = 0;
+        if (x < 0) {
+            x = Math.abs(x);
+            while (x > 0) {
+                int digit = x % 10;
+                x = x / 10;
+                sum = sum * 10 + digit;
+            }
+            sum = sum * -1;
+        } else if (x > 0) {
+            while (x > 0) {
+                int digit = x % 10;
+                x = x / 10;
+                sum = sum * 10 + digit;
+            }
+        }
+        if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE)
+            return 0;
+
+        return (int) sum;
+    }
+}
+
+// Palindrome Number
+class PalindromeNumberSolution {
+    public boolean isPalindrome(int x) {
+        int n = x;
+        int rev = 0;
+        if (x < 0) return false;
+
+        for (int i = x; i > 0; i /= 10) {
+            int lastDigit = i % 10;
+            rev = rev * 10 + lastDigit;
+        }
+
+        return (n == rev);
+    }
+}
+
+// LCM And GCD
+class LCMAndGCDSolution {
+    public static int[] lcmAndGcd(int a, int b) {
+        int[] res = new int[2];
+        res[0] = lcm(a, b);
+        res[1] = gcd(a, b);
+        return res;
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
+
+// Sum 1 to n Divisors
+class SumOfDivisorsSolution {
+    public static int sumOfDivisors(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += (n / i) * i;
+        }
+        return sum;
+    }
+}
+
+// Print 1 To N Without Loop
+class PrintNumbersSolution {
+    public void printNos(int n) {
+        if (n < 1)
+            return;
+        printNos(n - 1);
+        System.out.print(n + " ");
+    }
+}
+
+// Print GFG n times
+class PrintGFGSolution {
+    void printGfg(int N) {
+        if (N <= 0)
+            return;
+        printGfg(N - 1);
+        System.out.print("GFG ");
+    }
+}
+
+// Print N to 1 without loop
+class PrintNTo1Solution {
+    void printNos(int N) {
+        if (N < 1)
+            return;
+        System.out.print(N + " ");
+        printNos(N - 1);
+    }
+}
+
+// Sum of first n terms
+class SumOfSeriesSolution {
+    int sumOfSeries(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * n * n + sumOfSeries(n - 1);
+    }
+}
+
+// Factorials Less than or Equal to n
+class FactorialNumbersSolution {
+    static ArrayList<Long> factorialNumbers(long n) {
+        long val = 1;
+        int i = 1;
+        ArrayList<Long> res = new ArrayList<>();
+        while (val <= n) {
+            res.add(val);
+            i++;
+            val = val * i;
+        }
+        return res;
+    }
+}
+
+// Reverse an Array
+class ReverseArraySolution {
+    public void reverseArray(int arr[]) {
+        ReverseArraySolution obj = new ReverseArraySolution();
+        int n = arr.length;
+        obj.reverse(arr, 0, n - 1, n / 2);
+    }
+
+    public void reverse(int arr[], int start, int end, int mid) {
+        if (start == mid) {
+            return;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverse(arr, start + 1, end - 1, mid);
+    }
+}
+
+// Valid Palindrome
+class ValidPalindromeSolution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+    }
+}
+
+// Fibonacci Number
+class FibonacciSolution {
+    public int fib(int N) {
+        if (N <= 1)
+            return N;
+
+        int a = 0, b = 1;
+
+        while (N-- > 1) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
+}
+
+// Frequencies in a Limited Array
+class FrequencyCountSolution {
+    public List<Integer> frequencyCount(int[] arr) {
+        ArrayList<Integer> ar = new ArrayList<>();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (int i = 1; i <= arr.length; i++) {
+            if (map.containsKey(i)) {
+                ar.add(map.get(i));
+            } else {
+                ar.add(0);
+            }
+        }
+
+        return ar;
+    }
+}
+
+// Frequency of the Most Frequent Element
+class MaxFrequencySolution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0, right = 0;
+        int n = nums.length;
+        long sum = 0; 
+
+        while (right < n) {
+            sum += nums[right];
+
+            long requiredSum = (long) nums[right] * (right - left + 1);
+            long diff = requiredSum - sum;
+
+            if (diff > k) {
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+
+        return right - left;
     }
 }
