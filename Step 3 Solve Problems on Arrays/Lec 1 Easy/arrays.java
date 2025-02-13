ddacca3365f19af786d3401b589ac61f2fdcb712
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-13 23:57:58 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
@@ -56,3 +56,18 @@ class Solution {
         return count <= 1;
     }
 }
+
+// Remove Duplicates from Sorted Array
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
