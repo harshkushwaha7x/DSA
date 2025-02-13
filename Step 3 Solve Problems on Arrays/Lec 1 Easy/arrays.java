b3430d1a0c3789ecbc1128e1c484940e87361e38
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-13 23:56:02 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
@@ -10,4 +10,38 @@ class Solution {
     }
 }
 
-// 
+// Second Largest
+
+class Solution {
+    static void quickSort(int arr[], int low, int high) {
+        if(low<high){
+            int pIndex=partition(arr,low,high);
+            quickSort(arr,low,pIndex-1);
+            quickSort(arr,pIndex+1,high);
+        }
+        return;
+    }
+
+    static int partition(int arr[], int low, int high) {
+        int pivot=arr[low];
+        int i=low;
+        int j=high;
+        while(i<j){
+            while(arr[i]<=pivot && i<=high-1){
+                i++;
+            }
+            while(arr[j]>pivot && j>=low+1){
+                j--;
+            }
+            if(i<j){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[low];
+        arr[low]=arr[j];
+        arr[j]=temp;
+        return j;
+    }
+}
