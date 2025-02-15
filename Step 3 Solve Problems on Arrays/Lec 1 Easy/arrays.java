4a5ade870fa6df35f773c897aafc9b9d83f6050d
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-15 06:35:23 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
@@ -210,3 +210,22 @@ class Solution {
     }
 }
 
+// Max Consecutive Ones
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for(int val : nums){
+            if(val == 1){
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        return Math.max(count, max);
+    }
+}
+
+// 
