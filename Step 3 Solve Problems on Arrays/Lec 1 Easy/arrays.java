71a801fa51840bf1ceed9310496dccf9ffd324be
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-15 06:34:06 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
@@ -195,3 +195,18 @@ class UnionOfSortedArrays {
         return union;
     }
 }
+
+// Missing Number
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+       int exp_sum = n*(n + 1)/2;
+       int sum=0;
+       for(int i =0 ;i < n;i++){
+        sum+=nums[i];
+       }
+       return exp_sum - sum;
+    }
+}
+
