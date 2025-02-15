7fb6361a0393ed3a46d21282e45c2787f620cbf6
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-15 06:40:10 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 1 Easy/arrays.java
@@ -239,3 +239,28 @@ class SingleNumber {
     }
 }
 
+// Longest Subarray with Sum K
+class LongestSubarrayWithSumK {
+    public int longestSubarray(int[] arr, int k) {
+        Map<Integer, Integer> presum = new HashMap<>();
+        int n = arr.length;
+        int maxlen = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (sum == k) {
+                maxlen = Math.max(maxlen, i + 1);
+            }
+
+            int rem = sum - k;
+            if (presum.containsKey(rem)) {
+                int len = i - presum.get(rem);
+                maxlen = Math.max(maxlen, len);
+            }
+            if (!presum.containsKey(sum)) {
+                presum.put(sum, i);
+            }
+        }
+        return maxlen;
+    }
+}
