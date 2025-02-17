e50f4f54c35731e831c987f4520feffb9efa3bef
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-17 07:47:13 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
@@ -110,3 +110,36 @@ class Solution {
     }
 }
 
+// Next Permutation
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int len = nums.length;
+        int i = len - 2;
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+
+        if(i >= 0){
+            int j = len - 1;
+            while(nums[j] <= nums[i])
+                j--;
+            
+            swap(nums, i, j);
+        }
+        reverse(nums, i+1, len-1);
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void reverse(int[] nums, int st, int end){
+        while(st < end){
+            swap(nums, st, end);
+            st++; 
+            end--;
+        }
+    }
+}
