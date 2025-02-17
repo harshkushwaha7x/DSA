c065598e8b6d1fdd573c652ee8ef43f021092d20
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-17 07:44:41 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
@@ -50,3 +50,20 @@ class Solution {
     }
 }
 
+// Maximum Score from Subarray Minimums
+
+class Solution {
+
+    public int pairWithMaxSum(int arr[]) {
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+     
+        for(int i =0 ; i<n-1;i++){
+            int sum = arr[i] + arr[i+1];
+            if(sum> max){
+                max=sum;
+            }
+        }
+        return max;
+}
+}
