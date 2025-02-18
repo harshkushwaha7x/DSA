11a4a7e1a4a7dda13a26cc351785681f2c4920e1
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-18 22:51:39 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
@@ -223,3 +223,38 @@ class Solution {
         }
     }
 }
+
+// Spiral Matrix
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] mat) {
+        int m = mat.length, n=mat[0].length;
+        int srow=0, scol = 0,erow = m-1 , ecol=n-1;
+        ArrayList<Integer> ans= new ArrayList<>();
+        while(srow<=erow && scol<=ecol){
+            for (int j =scol; j<=ecol;j++){
+                ans.add(mat[srow][j]);
+            }
+
+            for(int i =srow+1; i<=erow; i++){
+                ans.add(mat[i][ecol]);
+            }
+            for(int j = ecol-1; j>=scol; j--){
+                if(srow==erow){
+                    break;
+                }
+                ans.add(mat[erow][j]);
+            }
+            for(int i =erow-1; i>=srow+1;i--){
+                if(scol==ecol){
+                    break;
+                }
+
+                ans.add(mat[i][scol]);
+            }
+            srow++; erow--; scol++; ecol--;
+        } 
+        return ans;
+    }
+}
+
