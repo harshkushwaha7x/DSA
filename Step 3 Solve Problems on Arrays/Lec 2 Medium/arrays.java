1e88a9e69efaa818431ece2011299886c73e319a
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-18 22:49:42 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
@@ -164,3 +164,38 @@ class Solution {
         return leaders;
     }
 }
+
+// Set Matrix Zeroes 
+
+import java.util.HashSet;
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        HashSet<Integer> zeroRows = new HashSet<>();
+        HashSet<Integer> zeroCols = new HashSet<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows.add(i);
+                    zeroCols.add(j);
+                }
+            }
+        }
+
+        for (int row : zeroRows) {
+            for (int j = 0; j < n; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+
+        for (int col : zeroCols) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] = 0;
+            }
+        }
+    }
+}
