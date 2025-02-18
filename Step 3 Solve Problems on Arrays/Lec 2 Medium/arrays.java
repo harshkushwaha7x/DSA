6d12c1d2aa3136f75def71b855e0c72c062532a0
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-18 22:58:13 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 2 Medium/arrays.java
@@ -1,11 +1,12 @@
-// Two Sum
+import java.util.*;
 
-class Solution {
+// Two Sum
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int val = target - nums[i];
-            if(map.containsKey(val)) {
+            if (map.containsKey(val)) {
                 return new int[]{map.get(val), i};
             }
             map.put(nums[i], i);
@@ -15,73 +16,67 @@ class Solution {
 }
 
 // Sort Colors
-
-class Solution {
+class SortColors {
     public void sortColors(int[] nums) {
-         Arrays.sort(nums);
+        Arrays.sort(nums);
     }
 }
 
 // Majority Element
-
-class Solution {
+class MajorityElement {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        return nums[n/2];
+        return nums[n / 2];
     }
 }
 
 // Maximum Subarray
-
-class Solution {
+class MaximumSubarray {
     public int maxSubArray(int[] nums) {
         int n = nums.length;
         int max = Integer.MIN_VALUE, sum = 0;
-        
-        for(int i=0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             sum += nums[i];
-            max = Math.max(sum,max);
-            
-            if(sum<0) sum = 0;
+            max = Math.max(sum, max);
+
+            if (sum < 0) sum = 0;
         }
-        
+
         return max;
     }
 }
 
 // Maximum Score from Subarray Minimums
-
-class Solution {
-
-    public int pairWithMaxSum(int arr[]) {
+class MaximumScoreFromSubarrayMinimums {
+    public int pairWithMaxSum(int[] arr) {
         int n = arr.length;
         int max = Integer.MIN_VALUE;
-     
-        for(int i =0 ; i<n-1;i++){
-            int sum = arr[i] + arr[i+1];
-            if(sum> max){
-                max=sum;
+
+        for (int i = 0; i < n - 1; i++) {
+            int sum = arr[i] + arr[i + 1];
+            if (sum > max) {
+                max = sum;
             }
         }
         return max;
-}
+    }
 }
 
 // Best Time to Buy and Sell Stock
-
-class Solution {
+class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
         int lsf = Integer.MAX_VALUE;
         int op = 0;
         int pist = 0;
-        
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < lsf) {
                 lsf = prices[i];
             }
             pist = prices[i] - lsf;
-            if(op < pist){
+            if (op < pist) {
                 op = pist;
             }
         }
@@ -90,18 +85,16 @@ class Solution {
 }
 
 // Rearrange Array Elements by Sign
-
-class Solution {
+class RearrangeArrayElementsBySign {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
         int pos = 0, neg = 1;
-        for(int i=0;i<n;i++) {
-            if(nums[i] >= 0) {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
                 ans[pos] = nums[i];
                 pos += 2;
-            }
-            else {
+            } else {
                 ans[neg] = nums[i];
                 neg += 2;
             }
@@ -111,43 +104,42 @@ class Solution {
 }
 
 // Next Permutation
-
-class Solution {
+class NextPermutation {
     public void nextPermutation(int[] nums) {
         int len = nums.length;
         int i = len - 2;
-        while(i >= 0 && nums[i] >= nums[i+1]){
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
 
-        if(i >= 0){
+        if (i >= 0) {
             int j = len - 1;
-            while(nums[j] <= nums[i])
+            while (nums[j] <= nums[i])
                 j--;
-            
+
             swap(nums, i, j);
         }
-        reverse(nums, i+1, len-1);
+        reverse(nums, i + 1, len - 1);
     }
 
-    private void swap(int[] nums, int i, int j){
+    private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    private void reverse(int[] nums, int st, int end){
-        while(st < end){
+
+    private void reverse(int[] nums, int st, int end) {
+        while (st < end) {
             swap(nums, st, end);
-            st++; 
+            st++;
             end--;
         }
     }
 }
 
 // Array Leaders
-
-class Solution {
-    static ArrayList<Integer> leaders(int arr[]) {
+class ArrayLeaders {
+    static ArrayList<Integer> leaders(int[] arr) {
         ArrayList<Integer> leaders = new ArrayList<>();
         int n = arr.length;
 
@@ -165,11 +157,8 @@ class Solution {
     }
 }
 
-// Set Matrix Zeroes 
-
-import java.util.HashSet;
-
-class Solution {
+// Set Matrix Zeroes
+class SetMatrixZeroes {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -201,8 +190,7 @@ class Solution {
 }
 
 // Rotate Image
-
-class Solution {
+class RotateImage {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
@@ -225,58 +213,55 @@ class Solution {
 }
 
 // Spiral Matrix
-
-class Solution {
+class SpiralMatrix {
     public List<Integer> spiralOrder(int[][] mat) {
-        int m = mat.length, n=mat[0].length;
-        int srow=0, scol = 0,erow = m-1 , ecol=n-1;
-        ArrayList<Integer> ans= new ArrayList<>();
-        while(srow<=erow && scol<=ecol){
-            for (int j =scol; j<=ecol;j++){
+        int m = mat.length, n = mat[0].length;
+        int srow = 0, scol = 0, erow = m - 1, ecol = n - 1;
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (srow <= erow && scol <= ecol) {
+            for (int j = scol; j <= ecol; j++) {
                 ans.add(mat[srow][j]);
             }
 
-            for(int i =srow+1; i<=erow; i++){
+            for (int i = srow + 1; i <= erow; i++) {
                 ans.add(mat[i][ecol]);
             }
-            for(int j = ecol-1; j>=scol; j--){
-                if(srow==erow){
+            for (int j = ecol - 1; j >= scol; j--) {
+                if (srow == erow) {
                     break;
                 }
                 ans.add(mat[erow][j]);
             }
-            for(int i =erow-1; i>=srow+1;i--){
-                if(scol==ecol){
+            for (int i = erow - 1; i >= srow + 1; i--) {
+                if (scol == ecol) {
                     break;
                 }
 
                 ans.add(mat[i][scol]);
             }
-            srow++; erow--; scol++; ecol--;
-        } 
-        return ans;
-    }
-}
-
-
-// Subarray Sum Equals K
-
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int sum = 0;
-        int ans = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,1);
-        for(int j=0;j<nums.length;j++){
-            sum += nums[j];
-            if(map.containsKey(sum -k)){
-                ans += map.get(sum-k);
-            }
-            map.put(sum,map.getOrDefault(sum,0)+1);
+            srow++;
+            erow--;
+            scol++;
+            ecol--;
         }
         return ans;
     }
 }
 
-
-
+// Subarray Sum Equals K
+class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0;
+        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return ans;
+    }
+}
