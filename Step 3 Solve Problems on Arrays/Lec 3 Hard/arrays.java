823e8e72fe430216fff0591a85095d19a95cd4ab
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-19 07:19:29 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 3 Hard/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 3 Hard/arrays.java
@@ -120,3 +120,27 @@ class Solution {
         return li;
     }
 }
+
+// Largest subarray with 0 sum
+
+class Solution {
+    int maxLen(int arr[]) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxlen = 0;
+        int Sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            Sum += arr[i];
+            if (Sum == 0) {
+                maxlen = i + 1; 
+            }
+            if (map.containsKey(Sum)) {
+                maxlen = Math.max(maxlen, i -map.get(Sum));
+            } else {
+                map.put(Sum, i);
+            }
+        }
+
+        return maxlen;
+    }
+}
+
