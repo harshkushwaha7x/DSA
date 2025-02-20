a52f4be7e59319a172f64c15a53ddfd727a91c05
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-20 16:05:26 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 3 Hard/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 3 Hard/arrays.java
@@ -215,3 +215,56 @@ class Solution {
         return ans;
     }
 }
+
+// Count Inversions
+
+class Solution {
+    static int inversionCount(int[] arr) {
+        if (arr.length < 2) {
+            return 0;
+        }
+        int[] temp = new int[arr.length];
+        return mergeSortAndCount(arr, temp, 0, arr.length - 1);
+    }
+
+    private static int mergeSortAndCount(int[] arr, int[] temp, int left, int right) {
+        int mid, invCount = 0;
+        if (left < right) {
+            mid = (left + right) / 2;
+            invCount += mergeSortAndCount(arr, temp, left, mid);
+            invCount += mergeSortAndCount(arr, temp, mid + 1, right);
+            invCount += mergeAndCount(arr, temp, left, mid, right);
+        }
+        return invCount;
+    }
+
+    private static int mergeAndCount(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        int invCount = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                invCount += (mid + 1) - i;
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i];
+        }
+
+        return invCount;
+    }
+}
