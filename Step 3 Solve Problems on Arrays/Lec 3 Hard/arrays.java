9186736ac43a18d377a541631709ab9620be0ff4
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-20 16:02:54 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 3 Hard/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 3 Hard/arrays.java
@@ -144,3 +144,26 @@ class Solution {
     }
 }
 
+// Merge Intervals
+
+class Solution {
+    public int[][] merge(int[][] arr) {
+        List<int[]> res = new ArrayList<>();
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0], b[0]));
+        int start = arr[0][0], prev_end = arr[0][1];
+
+        for(int i=1; i<arr.length; i++){
+            if( arr[i][0]<=prev_end ){
+                prev_end = Math.max(prev_end, arr[i][1]);
+            } 
+            else {
+                res.add(new int[]{start, prev_end});
+                start = arr[i][0];
+                prev_end = arr[i][1];
+            }
+        }
+        res.add(new int[]{start, prev_end});
+
+        return res.toArray(new int[res.size()][]);
+    }
+}
