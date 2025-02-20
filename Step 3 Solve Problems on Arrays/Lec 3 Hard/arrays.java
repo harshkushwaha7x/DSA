8ae4ce2637d86fdadf82ac22d15ef1a65a3a3650
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-20 16:04:51 +0530

    DSA commit

--- Step 3 Solve Problems on Arrays/Lec 3 Hard/arrays.java
+++ Step 3 Solve Problems on Arrays/Lec 3 Hard/arrays.java
@@ -187,4 +187,31 @@ class Solution {
  }
 
 
- // 
+ // Missing And Repeating
+
+ class Solution {
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        int freq[]=new int [arr.length+1];
+        int mis=-1;
+        int rep=-1;
+        for(int num:arr)
+        {
+            freq[num]++;
+        }
+        for(int i=1;i<=arr.length;i++)
+        {
+            if(freq[i]>=2)
+            {
+                rep=i;
+            }
+            if(freq[i]==0)
+            {
+                mis=i;
+            }
+        }
+        ArrayList<Integer>ans=new ArrayList<>();
+        ans.add(rep);
+        ans.add(mis);
+        return ans;
+    }
+}
