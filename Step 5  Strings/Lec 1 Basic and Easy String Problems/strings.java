b1a28b9b385957fbd1efbe66b801f46ccf64cbdf
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-23 18:35:21 +0530

    DSA commit

---
+++ Step 5  Strings/Lec 1 Basic and Easy String Problems/strings.java
@@ -0,0 +1,19 @@
+
+// Remove Outermost Parentheses
+
+class Solution {
+    public String removeOuterParentheses(String S) {
+    StringBuilder s = new StringBuilder();
+    int opened = 0;
+    for (char c : S.toCharArray()) {
+        if(c == '(') {
+            if(opened > 0) s.append(c);
+            opened++;
+        } else {
+            if(opened > 1) s.append(c);
+            opened--;
+        }
+    }
+    return s.toString();
+}
+}
