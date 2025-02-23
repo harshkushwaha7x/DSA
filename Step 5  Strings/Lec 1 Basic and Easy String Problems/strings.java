8bd83ea6b2182c17ee0037acca1ccc6cc609513d
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-23 18:42:46 +0530

    DSA commit

--- Step 5  Strings/Lec 1 Basic and Easy String Problems/strings.java
+++ Step 5  Strings/Lec 1 Basic and Easy String Problems/strings.java
@@ -34,6 +34,20 @@ class Solution {
     }
 }
 
+// Largest Odd Number in String
+
+class Solution {
+    public String largestOddNumber(String num) {
+
+        for(int i=num.length()-1; i>=0; i--){
+            if(num.charAt(i) % 2 == 1){
+                return num.substring(0, i+1);
+            }
+        }
+        return "";
+    }
+}
+
 // Longest Common Prefix
 
 class Solution {
