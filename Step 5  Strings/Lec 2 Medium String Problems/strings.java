84cc5f560d4f4f6e66b3b118e216514106111bf3
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-25 00:20:52 +0530

    DSA commit

--- Step 5  Strings/Lec 2 Medium String Problems/strings.java
+++ Step 5  Strings/Lec 2 Medium String Problems/strings.java
@@ -95,3 +95,45 @@ class Solution {
         return ans * sign;
     }
 }
+
+// Substrings with K Distinct
+
+
+class Solution {
+    
+ 
+    long countSubstring(String S, int K) {
+        int n = S.length();
+        int[] freq = new int[26];
+        int uc = 0;
+        long count = 0;
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (freq[S.charAt(right) - 'a'] == 0) {
+                uc++;
+            }
+            freq[S.charAt(right) - 'a']++;
+            
+            while (uc > K) {
+                freq[S.charAt(left) - 'a']--;
+                if (freq[S.charAt(left) - 'a'] == 0) {
+                    uc--;
+                }
+                left++;
+            }
+            count += right - left + 1;
+        }
+        
+        return count;
+    }
+    
+    
+    long substrCount(String S, int K) {
+        if (S == null || K <= 0) {
+            return 0;
+        }
+        
+        return countSubstring(S, K) - countSubstring(S, K - 1);
+    }
+}
