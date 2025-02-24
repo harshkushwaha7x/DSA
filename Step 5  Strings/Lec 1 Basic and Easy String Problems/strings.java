a573c30c899a7215e1c2203d80ef139b1cf5691a
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-24 23:54:57 +0530

    DSA commit

--- Step 5  Strings/Lec 1 Basic and Easy String Problems/strings.java
+++ Step 5  Strings/Lec 1 Basic and Easy String Problems/strings.java
@@ -95,3 +95,15 @@ class Solution {
         return comb.contains(goal);
     }
 }
+
+// Valid Anagram
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }
+}
