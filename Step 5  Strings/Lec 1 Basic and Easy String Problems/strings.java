efa88d90382632f957888e273b8c1072742be600
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-24 23:53:50 +0530

    DSA commit

--- Step 5  Strings/Lec 1 Basic and Easy String Problems/strings.java
+++ Step 5  Strings/Lec 1 Basic and Easy String Problems/strings.java
@@ -82,3 +82,16 @@ class Solution {
 		return true;
     }
 }
+
+// Rotate String
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        
+        String comb = s + s;
+        return comb.contains(goal);
+    }
+}
