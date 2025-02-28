e279abd8741c333867e22ab75339b575ed9fd6e4
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-02-28 23:53:43 +0530

    DSA commit

---
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 1 Learn 1D LinkedList/linkedlist.java
@@ -0,0 +1,15 @@
+// Array to Linked List
+
+class Solution {
+    static Node constructLL(int arr[]) {
+        Node head= new Node(arr[0]);
+        Node mover = head;
+        for(int i=1;i<arr.length;i++)
+        {
+            Node temp= new Node(arr[i]);
+            mover.next=temp;
+            mover=temp;
+        }
+        return head;
+    }
+}
