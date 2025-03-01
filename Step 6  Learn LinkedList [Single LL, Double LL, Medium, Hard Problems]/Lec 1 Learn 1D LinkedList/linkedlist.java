c0cf701709978e70e7abd6e039a52b5850c61388
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-01 20:46:34 +0530

    DSA commit

--- Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 1 Learn 1D LinkedList/linkedlist.java
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 1 Learn 1D LinkedList/linkedlist.java
@@ -1,40 +1,36 @@
 // Array to Linked List
-
-class Solution {
+class ArrayToLinkedList {
     static Node constructLL(int arr[]) {
-        Node head= new Node(arr[0]);
+        Node head = new Node(arr[0]);
         Node mover = head;
-        for(int i=1;i<arr.length;i++)
-        {
-            Node temp= new Node(arr[i]);
-            mover.next=temp;
-            mover=temp;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
         }
         return head;
     }
 }
 
 // Linked List Insertion At End
-
-class Solution {
+class LinkedListInsertion {
     Node insertAtEnd(Node head, int x) {
-        if(head==null){
+        if (head == null) {
             return new Node(x);
         }
-        
-        Node newNode=new Node(x);
-        Node temp=head;
-        while(temp.next!=null){
-            temp=temp.next;
+
+        Node newNode = new Node(x);
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
         }
-        temp.next=newNode;
+        temp.next = newNode;
         return head;
     }
 }
 
 // Delete Node in a Linked List
-
-class Solution {
+class DeleteNode {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
@@ -42,35 +38,50 @@ class Solution {
 }
 
 // Count Linked List Nodes
-
-class Solution{
-    public int getcount(Node head){
-        int count=0;
-        Node current=head;
-        while(current!=null){
+class CountNodes {
+    public int getCount(Node head) {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
             count++;
-            current=current.next;
+            current = current.next;
         }
         return count;
     }
 }
 
 // Search in Linked List
-
-class Solution {
+class SearchInLinkedList {
     static boolean searchKey(int n, Node head, int key) {
-        boolean present = false;
         Node counter = head;
-        while(counter.next!=null){
-            
-            if(counter.data == key){
-                present = true;
-                
+        while (counter != null) {
+            if (counter.data == key) {
+                return true;
             }
             counter = counter.next;
         }
-       
-        return present;
+        return false;
     }
 }
 
+// Node class definition
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// ListNode class definition (if needed)
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
