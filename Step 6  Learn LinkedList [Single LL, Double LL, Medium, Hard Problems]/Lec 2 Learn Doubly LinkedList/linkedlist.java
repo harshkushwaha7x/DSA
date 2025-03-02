34a32d9106366356ca28d7cafef9b6d08f761c25
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-02 17:31:29 +0530

    DSA commit

--- Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 2 Learn Doubly LinkedList/linkedlist.java
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 2 Learn Doubly LinkedList/linkedlist.java
@@ -1,95 +1,83 @@
 // Introduction to Doubly Linked List
 
-Class Solution {
+class DoublyLinkedList {
     Node constructDLL(int arr[]) {
-        Node head=null;
-        Node  tail=null;
-        for(int x:arr){
-            if(head==null){
-                head=insertEnd(tail,x);
-                tail=head;
-            }else{
-                tail=insertEnd(tail,x);
+        Node head = null;
+        Node tail = null;
+        for (int x : arr) {
+            if (head == null) {
+                head = insertEnd(tail, x);
+                tail = head;
+            } else {
+                tail = insertEnd(tail, x);
             }
         }
         return head;
     }
-    Node insertEnd(Node tail,int x){
-        Node temp=new Node(x);
-        if(tail==null){
+
+    Node insertEnd(Node tail, int x) {
+        Node temp = new Node(x);
+        if (tail == null) {
             return temp;
         }
-        tail.next=temp;
-        temp.prev=tail;
+        tail.next = temp;
+        temp.prev = tail;
         return temp;
     }
-    
 }
 
 // Doubly linked list Insertion at given position
 
-class Solution {
+class DoublyLinkedListInsertion {
     Node addNode(Node head, int p, int x) {
         Node node = new Node(x);
-        Node temp =head;
-        while(p!=0){
+        Node temp = head;
+        while (p != 0) {
             temp = temp.next;
             p--;
         }
         node.next = temp.next;
-        temp.next= node;
+        temp.next = node;
         node.prev = temp;
-        
-        
+
         return head;
     }
 }
 
- // Delete in a Doubly Linked List
+// Delete in a Doubly Linked List
 
- class Solution {
-    public Node deleteNode(Node head, int x) 
-    {
-        git commit -m "DSA commit"
-        if(head == null) return null;
-        
-        if(x == 1)
-        {
+class DoublyLinkedListDeletion {
+    public Node deleteNode(Node head, int x) {
+        if (head == null) return null;
+
+        if (x == 1) {
             head = head.next;
-            if(head != null)
-            {
+            if (head != null) {
                 head.prev = null;
             }
             return head;
         }
-        
+
         Node temp = head;
-        
+
         int count = 1;
-        while(temp != null)
-        {
-            if(count == x) break;
+        while (temp != null) {
+            if (count == x) break;
             temp = temp.next;
             count++;
         }
-        
+
         Node back = temp.prev;
         Node front = temp.next;
-        
-        if(temp.next == null)
-        {
+
+        if (temp.next == null) {
             temp.prev = null;
             back.next = null;
-        }
-        
-        else if(temp.prev == null)
-        {
+        } else if (temp.prev == null) {
             head = head.next;
             temp.next = null;
             head.prev = null;
-        }
-        else
-        {
+        } else {
             back.next = front;
             front.prev = back;
             temp.next = null;
@@ -101,10 +89,10 @@ class Solution {
 
 // Reverse a Doubly Linked List
 
-class Solution {
+class DoublyLinkedListReversal {
     public DLLNode reverseDLL(DLLNode head) {
-      if (head == null || head.next == null) {
-            return head; 
+        if (head == null || head.next == null) {
+            return head;
         }
         DLLNode current = head;
         DLLNode last = null;
@@ -115,5 +103,5 @@ class Solution {
             current = current.prev;
         }
         return last.prev;
-        }
+    }
 }
