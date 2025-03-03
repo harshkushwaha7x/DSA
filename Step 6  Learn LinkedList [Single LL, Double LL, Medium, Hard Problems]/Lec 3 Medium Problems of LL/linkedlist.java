c3469da9d8467b89a574894374f5b1bc515afb08
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-03 17:49:40 +0530

    DSA commit

--- Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
@@ -50,3 +50,26 @@ class Solution {
     }
 }
 
+// Linked List Cycle
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
