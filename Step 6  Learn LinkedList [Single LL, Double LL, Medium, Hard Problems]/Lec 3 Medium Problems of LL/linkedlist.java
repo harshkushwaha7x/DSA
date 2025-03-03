5c1c3861f29d0f91991417f875de8b5f075a8f8c
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-03 17:48:59 +0530

    DSA commit

--- Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
@@ -32,3 +32,21 @@ class Solution {
        return prev; 
     }
 }
+
+//  Reverse Linked List
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nextNode = null;
+        while (curr != null) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
+
