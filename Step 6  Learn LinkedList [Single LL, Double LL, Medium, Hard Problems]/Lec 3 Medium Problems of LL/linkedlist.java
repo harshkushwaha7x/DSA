d9c8cccc6741c7a66fb2e0e2f284ce83485195b6
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-04 23:45:38 +0530

    DSA commit

--- Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
@@ -176,3 +176,24 @@ class Solution {
     }
 }
 
+// Remove Nth Node From End of List
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        for(int i=0; i<n ; i++){
+            fast = fast.next;
+        }
+        if(fast == null){
+            head = head.next;
+            return head;
+        }
+        ListNode slow = head;
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
