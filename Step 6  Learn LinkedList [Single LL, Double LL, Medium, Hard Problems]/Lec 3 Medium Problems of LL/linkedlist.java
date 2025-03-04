73433a777708e827c313fdc8fac7948b9086bc6c
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-04 23:46:13 +0530

    DSA commit

--- Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
@@ -197,3 +197,21 @@ class Solution {
         return head;
     }
 }
+
+// Delete the Middle Node of a Linked List
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast.next != null && fast.next.next != null){
+           fast = fast.next.next;
+           slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
