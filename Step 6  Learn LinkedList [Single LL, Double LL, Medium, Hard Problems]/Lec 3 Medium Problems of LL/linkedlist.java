d4deae37c009db5567d6922f02d0a426b1008265
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-04 23:48:42 +0530

    DSA commit

--- Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/linkedlist.java
@@ -264,3 +264,35 @@ public class Solution {
     }
   
   }
+
+  // Sort a linked list of 0s, 1s and 2s
+
+  class Solution {
+    static Node segregate(Node head) {
+        Node zeroHead=new Node(-1);
+        Node oneHead=new Node(-1);
+        Node twoHead=new Node(-1);
+        Node zero=zeroHead;
+        Node one=oneHead;
+        Node two=twoHead;
+        
+        Node temp = head;
+        while(temp != null){
+              if(temp.data==0){
+                  zero.next=temp;
+                  zero=zero.next;
+              }else if(temp.data==1){
+                  one.next=temp;
+                  one=one.next;
+              }else{
+                  two.next=temp;
+                  two=two.next;
+              }
+              temp=temp.next;
+        }
+        zero.next=(oneHead.next!=null)?oneHead.next:twoHead.next;
+         one.next=twoHead.next;
+          two.next=null;
+          return zeroHead.next;
+    }
+}
