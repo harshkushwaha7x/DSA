c1e4db778d78d725e4dc4d1bfc1193175d767a58
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-06 23:53:25 +0530

    DSA commit

--- Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 4 Medium Problems of DLL/linkedlist.java
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 4 Medium Problems of DLL/linkedlist.java
@@ -1,16 +1,15 @@
-// Delete all occurrences of a given key in a doubly linked list
-
-class Solution {
+// Class to delete all occurrences of a given key in a doubly linked list
+class DeleteOccurrences {
     static Node deleteAllOccurOfX(Node head, int x) {
         Node dummy = new Node();
         dummy.next = head;
         Node cur = head;
         Node prev = dummy;
-        
-        while(cur != null){
-            if(cur.data == x){
+
+        while (cur != null) {
+            if (cur.data == x) {
                 prev.next = cur.next;
-                if(cur.next != null){
+                if (cur.next != null) {
                     cur.next.prev = prev;
                 }
                 cur = prev.next;
@@ -23,53 +22,54 @@ class Solution {
     }
 }
 
-// Find pairs with given sum in doubly linked list
-
-class Solution {
+// Class to find pairs with a given sum in a doubly linked list
+class FindPairs {
     public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, Node head) {
-        Node last=head;
-        Node first=head;
-        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
-        if(head==null || head.next==null)return ans;
-        while(last.next!=null){
-            last=last.next;
+        Node last = head;
+        Node first = head;
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        if (head == null || head.next == null) return ans;
+
+        while (last.next != null) {
+            last = last.next;
         }
-        while(first.data<last.data){
-            ArrayList<Integer>temp=new ArrayList<>();
-            if(first.data+last.data==target){
+
+        while (first.data < last.data) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            if (first.data + last.data == target) {
                 temp.add(first.data);
                 temp.add(last.data);
                 ans.add(temp);
-                first=first.next;
-                last=last.prev;
+                first = first.next;
+                last = last.prev;
+            } else if (first.data + last.data > target) {
+                last = last.prev;
+            } else {
+                first = first.next;
             }
-            else if(first.data+last.data>target)
-            last=last.prev;
-            else
-            first=first.next;
         }
         return ans;
     }
 }
 
-// Remove duplicates from a sorted doubly linked list
-
-class Solution {
+// Class to remove duplicates from a sorted doubly linked list
+class RemoveDuplicates {
     Node removeDuplicates(Node head) {
-        if (head == null || head.next == null) return head; 
+        if (head == null || head.next == null) return head;
+
         Node temp = head;
         while (temp != null && temp.next != null) {
             Node t = temp;
             while (t.next != null && t.data == t.next.data) {
-                t = t.next;  
+                t = t.next;
             }
 
-            temp.next = t.next;  
+            temp.next = t.next;
             if (t.next != null) {
-                t.next.prev = temp;  
+                t.next.prev = temp;
             }
 
-            temp = temp.next;  
+            temp = temp.next;
         }
         return head;
     }
