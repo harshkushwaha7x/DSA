4017a138bc30528d106b41f7a08cf1ee6137688a
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-07 22:54:24 +0530

    DSA commit

---
+++ Step 6  Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 5 Hard Problems of LL/linkedlist.java
@@ -0,0 +1,45 @@
+// Reverse Nodes in k-Group
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) {
+            return head;
+        }
+
+        int size = getListSize(head);
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (size >= k) {
+            ListNode curr = prev.next;
+            ListNode next = null;
+            ListNode subPrev = null;
+
+            for (int i = 0; i < k; i++) {
+                next = curr.next;
+                curr.next = subPrev;
+                subPrev = curr;
+                curr = next;
+            }
+
+            ListNode start = prev.next; 
+            prev.next = subPrev;
+            start.next = curr;
+
+            prev = start;
+            size -= k;
+        }
+
+        return dummy.next;
+    }
+
+    private int getListSize(ListNode head) {
+        int size = 0;
+        while (head != null) {
+            size++;
+            head = head.next;
+        }
+        return size;
+    }
+}
