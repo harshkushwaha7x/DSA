7e58b3e780921d1e63b11ff6e47af62ae70fd1db
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-09 22:56:23 +0530

    DSA commit

--- Step 7  Recursion [PatternWise]/Lec 2 Subsequences Pattern/recursion.java
+++ Step 7  Recursion [PatternWise]/Lec 2 Subsequences Pattern/recursion.java
@@ -111,3 +111,35 @@ class Solution {
 
 // Better String
 
+class Solution {
+    public static String betterString(String str1, String str2) {
+       HashMap<Character,Integer>mpp1=new HashMap<>();
+       HashMap<Character,Integer>mpp2=new HashMap<>();
+       int dp1[]=new int [str1.length()+1];
+       int dp2[]=new int [str2.length()+1];
+       dp1[0]=1;
+       dp2[0]=1;
+       for (int i=0;i<str1.length();i++){
+           if (mpp1.containsKey(str1.charAt(i))){
+               dp1[i+1]=(2*dp1[i])-dp1[mpp1.get(str1.charAt(i))-1];
+           }else{
+               dp1[i+1]=dp1[i]*2;
+           }
+            mpp1.put(str1.charAt(i),i+1);
+       }
+       for (int i=0;i<str2.length();i++){
+           if (mpp2.containsKey(str2.charAt(i))){
+               dp2[i+1]=(2*dp2[i])-dp2[mpp2.get(str2.charAt(i))-1];
+           }else{
+               dp2[i+1]=dp2[i]*2;
+           }
+           mpp2.put(str2.charAt(i),i+1);
+       }
+       int len1=str1.length();
+       int len2=str2.length();
+       if (dp1[len1]>=dp2[len2]){
+           return str1;
+       }
+       return str2;
+    }
+}
