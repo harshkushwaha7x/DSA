73a4e4b5b7bf814ffe93c440fa1f0c3c278c3ed6
Harsh Kushwaha <harshkushwaha4151@gmail.com>
2025-03-10 23:21:44 +0530

    DSA commit

--- Step 7  Recursion [PatternWise]/Lec 2 Subsequences Pattern/recursion.java
+++ Step 7  Recursion [PatternWise]/Lec 2 Subsequences Pattern/recursion.java
@@ -247,3 +247,27 @@ class Solution {
     }
 }
 
+// Subsets II
+
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public void solve(int idx, int[] nums, List<Integer> opList){
+        if(idx == nums.length){
+            ans.add(new ArrayList<>(opList));
+            return;
+        }
+        opList.add(nums[idx]);
+        solve(idx+1,nums,opList);
+        opList.remove(opList.size()-1);
+        while(idx+1<nums.length && nums[idx]==nums[idx+1]) 
+            idx++;
+        solve(idx+1,nums,opList); 
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums); 
+        solve(0, nums, new ArrayList<>());
+        return ans;
+    }
+}
