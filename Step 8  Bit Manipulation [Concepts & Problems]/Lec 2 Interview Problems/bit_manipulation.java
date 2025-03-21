3850f670883740b556189e3f4d4d237c092f008e
itachi <harshkushwaha4151@gmail.com>
2025-03-22 00:06:25 +0530

    DSA commit

---
+++ Step 8  Bit Manipulation [Concepts & Problems]/Lec 2 Interview Problems/bit_manipulation.java
@@ -0,0 +1,17 @@
+// Minimum Bit Flips to Convert Number
+
+class Solution {
+    public int minBitFlips(int start, int goal) {
+      var res = 0;
+  
+      while (start > 0 || goal > 0) {
+        res += (start & 1) == (goal & 1) ? 0 : 1;
+  
+        start >>= 1;
+        goal >>= 1;
+      }
+      return res;
+    }
+  }
+
+  
