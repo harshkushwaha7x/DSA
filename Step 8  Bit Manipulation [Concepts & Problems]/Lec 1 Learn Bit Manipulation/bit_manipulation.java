58a1021511b956606360e7a4354306d8dc0969b7
itachi <harshkushwaha4151@gmail.com>
2025-03-21 23:55:42 +0530

    DSA commit

--- Step 8  Bit Manipulation [Concepts & Problems]/Lec 1 Learn Bit Manipulation/bit_manipulation.java
+++ Step 8  Bit Manipulation [Concepts & Problems]/Lec 1 Learn Bit Manipulation/bit_manipulation.java
@@ -1,29 +1,28 @@
 // Bit Manipulation
+import java.math.BigInteger;
+import java.util.List;
+import java.util.ArrayList;
 
-class Solution {
+class BitManipulation {
     static void bitManipulation(int num, int i) {
-        int get=(num&(1<<i-1))!=0?1:0;
-        int set=num|(1<<i-1);
-        int clear=num&~(1<<i-1);
-        System.out.print(get+" "+set+" "+clear);
+        int get = (num & (1 << (i - 1))) != 0 ? 1 : 0;
+        int set = num | (1 << (i - 1));
+        int clear = num & ~(1 << (i - 1));
+        System.out.print(get + " " + set + " " + clear);
     }
 }
 
 // K-th Bit is Set or Not
 
-class CheckBit {
+class CheckKthBit {
     static boolean checkKthBit(int n, int k) {
-        StringBuilder str=new StringBuilder();
-        while(n!=1){
-            str.append(n%2);
-            n=n/2;
+        StringBuilder str = new StringBuilder();
+        while (n != 0) {
+            str.append(n % 2);
+            n = n / 2;
         }
-        for(int i=0;i<str.length();i++){
-            if(i==k){
-                if(str.charAt(i)=='1'){
-                    return true;
-                }
-            }
+        if (k < str.length() && str.charAt(k) == '1') {
+            return true;
         }
         return false;
     }
@@ -31,77 +30,67 @@ class CheckBit {
 
 // Odd or Even
 
-class Solution {
+class OddEvenChecker {
     static boolean isEven(int n) {
-          
-          if((n&1)==0){
-              return true;
-          }
-          
-          return false;
+        return (n & 1) == 0;
     }
 }
 
 // Power of Two
 
-class Solution {
+class PowerOfTwoChecker {
     public boolean isPowerOfTwo(int n) {
-        if(n==0){
+        if (n == 0) {
             return false;
         }
-        while(n%2==0){
-            n/=2;
+        while (n % 2 == 0) {
+            n /= 2;
         }
-        if(n==1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return n == 1;
     }
 }
 
 // Count total set bits
 
-class Solution{ 
-    private static int findx(int n){
-        int x=0;
-        while((1<<x)<=n)x++;
-        return x-1;
+class CountSetBits {
+    private static int findx(int n) {
+        int x = 0;
+        while ((1 << x) <= n) x++;
+        return x - 1;
     }
-    public static int countSetBits(int n){
-        if(n==0)return 0;
-        int x=findx(n);
-        int bitstill2x=x*(1<<(x-1));
-        int msb=n-(1<<x)+1;
-        int rest=n-(1<<x);
-        int ans=bitstill2x+msb+countSetBits(rest);
+
+    public static int countSetBits(int n) {
+        if (n == 0) return 0;
+        int x = findx(n);
+        int bitstill2x = x * (1 << (x - 1));
+        int msb = n - (1 << x) + 1;
+        int rest = n - (1 << x);
+        int ans = bitstill2x + msb + countSetBits(rest);
         return ans;
     }
 }
 
 // Divide Two Integers
 
-class Solution {
+class DivideTwoIntegers {
     public int divide(int dividend, int divisor) {
-        if(dividend==-2147483648 && divisor ==-1) return Integer.MAX_VALUE;
-       BigInteger divden = new BigInteger(Integer.toString(dividend));
-       BigInteger divsor = new BigInteger(Integer.toString(divisor));
-       BigInteger ans = divden.divide(divsor);
-       int res = ans.intValue();
-       return res;
+        if (dividend == -2147483648 && divisor == -1) return Integer.MAX_VALUE;
+        BigInteger divden = new BigInteger(Integer.toString(dividend));
+        BigInteger divsor = new BigInteger(Integer.toString(divisor));
+        BigInteger ans = divden.divide(divsor);
+        int res = ans.intValue();
+        return res;
     }
 }
 
 // Swap two numbers
 
-class Solution{
-    static List<Integer> get(int a,int b)
-    {
-        List<Integer> res=new ArrayList<>();
-        a=a^b;
-        b=a^b;
-        a=a^b;
+class SwapNumbers {
+    static List<Integer> get(int a, int b) {
+        List<Integer> res = new ArrayList<>();
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
         res.add(a);
         res.add(b);
         return res;
@@ -110,7 +99,7 @@ class Solution{
 
 // Set the rightmost unset bit
 
-class Solution {
+class SetRightmostUnsetBit {
     static int setBit(int n) {
         return (n | (n + 1));
     }
