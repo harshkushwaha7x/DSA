5f678dbbe70c78dd2e1fd3b02698736200b9e496
itachi <harshkushwaha4151@gmail.com>
2025-03-22 00:06:58 +0530

    DSA commit

--- Step 8  Bit Manipulation [Concepts & Problems]/Lec 2 Interview Problems/bit_manipulation.java
+++ Step 8  Bit Manipulation [Concepts & Problems]/Lec 2 Interview Problems/bit_manipulation.java
@@ -14,4 +14,14 @@ class Solution {
     }
   }
 
-  
+  // Single Number
+
+  class Solution {
+    public int singleNumber(int[] nums) {
+        int xorr = 0;
+        for(int i=0; i<nums.length; i++){
+            xorr = xorr ^ nums[i];
+        }
+        return xorr;
+    }
+}
