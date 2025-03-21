c1e35329c204de8a76b8cc4a1d930538d6cc2838
itachi <harshkushwaha4151@gmail.com>
2025-03-21 23:51:11 +0530

    DSA commit

--- Step 8  Bit Manipulation [Concepts & Problems]/Lec 1 Learn Bit Manipulation/bit_manipulation.java
+++ Step 8  Bit Manipulation [Concepts & Problems]/Lec 1 Learn Bit Manipulation/bit_manipulation.java
@@ -80,3 +80,15 @@ class Solution{
     }
 }
 
+// Divide Two Integers
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend==-2147483648 && divisor ==-1) return Integer.MAX_VALUE;
+       BigInteger divden = new BigInteger(Integer.toString(dividend));
+       BigInteger divsor = new BigInteger(Integer.toString(divisor));
+       BigInteger ans = divden.divide(divsor);
+       int res = ans.intValue();
+       return res;
+    }
+}
