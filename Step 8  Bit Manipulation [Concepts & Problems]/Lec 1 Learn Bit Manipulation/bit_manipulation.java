466fc8c7fdf546fae459dd09de0b910a717e0f36
itachi <harshkushwaha4151@gmail.com>
2025-03-21 23:52:03 +0530

    DSA commit

--- Step 8  Bit Manipulation [Concepts & Problems]/Lec 1 Learn Bit Manipulation/bit_manipulation.java
+++ Step 8  Bit Manipulation [Concepts & Problems]/Lec 1 Learn Bit Manipulation/bit_manipulation.java
@@ -92,3 +92,18 @@ class Solution {
        return res;
     }
 }
+
+// Swap two numbers
+
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        List<Integer> res=new ArrayList<>();
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        res.add(a);
+        res.add(b);
+        return res;
+    }
+}
