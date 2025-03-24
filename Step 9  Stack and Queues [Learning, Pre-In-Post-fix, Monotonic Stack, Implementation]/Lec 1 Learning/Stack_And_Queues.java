48746751bca25c1f2af7dcf928560730e7ac685e
itachi <harshkushwaha4151@gmail.com>
2025-03-24 22:48:31 +0530

    DSA commit

--- Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 1 Learning/Stack_And_Queues.java
+++ Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 1 Learning/Stack_And_Queues.java
@@ -18,3 +18,27 @@ class MyStack {
         return -1;
     }
 }
+
+// Queue Using Array
+
+class MyQueue {
+ 
+    int front, rear;
+int arr[] = new int[100005];
+ 
+    MyQueue()
+{
+front=0;
+rear=0;
+}
+ 
+void push(int x)
+{
+    arr[rear++]=x;
+} 
+ 
+int pop() {
+        return (front == rear) ? -1 : arr[front++];
+    }
+ 
+}
