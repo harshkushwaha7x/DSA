55d5caefa7ef8d3eefa41841cc9f8e7c9f9dc2da
itachi <harshkushwaha4151@gmail.com>
2025-03-26 23:43:46 +0530

    DSA commit

--- Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 2 Prefix, Infix, PostFix Conversion Problems/Stack_And_Queues.java
+++ Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 2 Prefix, Infix, PostFix Conversion Problems/Stack_And_Queues.java
@@ -49,3 +49,21 @@ class Solution {
         return stack.pop();
     }
 }
+
+// Prefix to Postfix Conversion
+
+class Solution {
+    static String preToPost(String pre_exp) {
+        Stack<String> stack=new Stack<>();
+        for(int i=pre_exp.length()-1;i>=0;i--){
+            char ch=pre_exp.charAt(i);
+            if(Character.isLetterOrDigit(ch))stack.push(String.valueOf(ch));
+            else{
+                String t1=stack.pop();
+                String t2=stack.pop();
+                stack.push(t1+t2+ch);
+            }
+        }
+        return stack.pop();
+    }
+}
