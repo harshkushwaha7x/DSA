2d806a67ceb4cece08eeb947ce5adebbe30ffa9d
itachi <harshkushwaha4151@gmail.com>
2025-03-28 22:43:14 +0530

    DSA commit

--- Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 Monotonic StackQueue Problems [VVV. Imp]/Stack_And_Queues.java
+++ Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 Monotonic StackQueue Problems [VVV. Imp]/Stack_And_Queues.java
@@ -33,3 +33,26 @@ class Solution {
         
     }
 }
+
+// Next Greater Element II
+
+class Solution {
+    public int[] nextGreaterElements(final int[] nums) {
+        final int n = nums.length;
+        final int[] res = new int[n];
+        final Stack<Integer> stack = new Stack<>();
+
+        Arrays.fill(res, -1);
+
+        for(int i = 0; i < n * 2; ++i) {
+            final int idx = i % n;
+
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[idx])
+                res[stack.pop()] = nums[idx];
+
+            stack.push(idx);
+        }
+
+        return res;
+    }
+}
