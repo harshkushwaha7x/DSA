fa9b4c2a52af613c51fa41149fe6f162eae91f9d
itachi <harshkushwaha4151@gmail.com>
2025-03-29 00:16:14 +0530

    DSA commit

--- Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 Monotonic StackQueue Problems [VVV. Imp]/Stack_And_Queues.java
+++ Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 3 Monotonic StackQueue Problems [VVV. Imp]/Stack_And_Queues.java
@@ -206,3 +206,22 @@ class Solution {
         return result;
     }
 }
+
+// Sum of Subarray Ranges
+
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        long ans = 0;
+        for(int i=0; i<n; i++){
+            int max = nums[i];
+            int min = nums[i];
+            for(int j=i; j<n; j++){
+                max = Math.max(nums[j],max);
+                min = Math.min(nums[j],min);
+                ans += max-min;
+            }
+        }
+        return ans;
+    }
+}
