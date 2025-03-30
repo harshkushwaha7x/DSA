d9ed6da866d4b89449baa701b8d119bf9f6377b4
itachi <harshkushwaha4151@gmail.com>
2025-03-30 10:57:10 +0530

    DSA commit

--- Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 4 Implementation Problems/Stack_And_Queues.java
+++ Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 4 Implementation Problems/Stack_And_Queues.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+import java.util.Map;
+import java.util.HashMap;
 
 // Sliding Window Maximum
 
@@ -51,3 +53,74 @@ class StockSpanner {
         return ans;
     }
 }
+
+// LRU Cache
+
+class LRUCache {
+    private int capacity;
+    private Map<Integer,Node> map;
+    private Node tail, head;
+    class Node{
+        int key,value;
+        Node prev, next;
+        Node(int key, int value){
+            this.key=key;
+            this.value=value;
+        }
+    }
+    public LRUCache(int capacity) {
+        this.capacity=capacity;
+        map=new HashMap<>();
+        head=new Node(0,0);
+        tail=new Node(0,0);
+        head.next=tail; 
+        tail.prev=head; 
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)){
+            Node node=map.get(key);
+            remove(node);
+            addToFront(node);
+            return node.value;
+        }
+        return -1;
+    }
+    private void remove(Node node){
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
+    }
+    private void addToFront(Node node){
+        node.next=head.next;
+        node.prev=head;
+        head.next.prev=node;
+        head.next=node;
+    }
+    private void moveToHead(Node node){
+        remove(node);
+        addToFront(node);
+    }
+    private Node removeTail(){
+        Node lru=tail.prev;
+        remove(lru);
+        return lru;
+    }
+    public void put(int key, int value) {
+         if(map.containsKey(key)){
+            Node node=map.get(key);
+            node.value=value;
+            moveToHead(node);
+         }
+         else{
+            Node newNode=new Node(key,value);
+            addToFront(newNode);
+            map.put(key,newNode);
+
+            if(map.size()>capacity){
+                Node lru=removeTail();
+                map.remove(lru.key);
+            }
+         }
+    }
+}
+
