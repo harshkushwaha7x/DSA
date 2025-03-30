50227f070ef524e7ae2433b067bb8203b4d507eb
itachi <harshkushwaha4151@gmail.com>
2025-03-30 11:02:47 +0530

    DSA commit

--- Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 4 Implementation Problems/Stack_And_Queues.java
+++ Step 9  Stack and Queues [Learning, Pre-In-Post-fix, Monotonic Stack, Implementation]/Lec 4 Implementation Problems/Stack_And_Queues.java
@@ -1,6 +1,8 @@
 import java.util.Stack;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.TreeMap;
 
 // Sliding Window Maximum
 
@@ -121,6 +123,105 @@ class LRUCache {
                 map.remove(lru.key);
             }
          }
+    }
+}
+
+// LFU Cache
+
+class LFUCache {
+
+    private final Map<Integer, CachedElement<Integer, Integer>> elements;
+
+    private final TreeMap<Integer, LinkedList<Integer>> frequencies = new TreeMap<>();
+
+    private final int capacity;
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        this.elements = new HashMap<>(capacity);
+    }
+
+    public int get(int key) {
+        CachedElement<Integer, Integer> element = this.elements.get(key);
+        if (element == null) {
+            return -1;
+        }
+
+        incrementFrequency(element);
+
+        return element.value;
+    }
+
+    public void put(int key, int value) {
+        if (!this.elements.containsKey(key) && this.capacity == this.elements.size()) {
+            this.removeLFU();
+        }
+
+        CachedElement<Integer, Integer> element = this.elements.get(key);
+        if (element == null) {
+            element = new CachedElement<>(key, value);
+            this.addNew(element);
+        } else {
+            element.value = value;
+            incrementFrequency(element);
+        }
+
+        this.elements.put(key, element);
+    }
+
+    private void addNew(CachedElement<Integer, Integer> element) {
+        LinkedList<Integer> elementsOfOneFrequency = this.frequencies.getOrDefault(
+                element.accessCount,
+                new LinkedList<>()
+        );
+        elementsOfOneFrequency.addFirst(element.key);
+
+        this.frequencies.put(element.accessCount, elementsOfOneFrequency);
+    }
+
+    private void removeLFU() {
+        int elementsOfMinFrequencyKey = frequencies.firstKey();
+        LinkedList<Integer> elementsOfMinFrequency = frequencies.remove(elementsOfMinFrequencyKey);
+        int lfuKey = elementsOfMinFrequency.removeLast();
+
+        if (!elementsOfMinFrequency.isEmpty()) {
+            this.frequencies.put(elementsOfMinFrequencyKey, elementsOfMinFrequency);
+        }
+
+        this.elements.remove(lfuKey);
+    }
+
+    private void incrementFrequency(CachedElement<Integer, Integer> element) {
+        LinkedList<Integer> elementsOfCurrentFrequency = this.frequencies.remove(element.accessCount);
+        elementsOfCurrentFrequency.remove(element.key);
+
+        if (!elementsOfCurrentFrequency.isEmpty()) {
+            this.frequencies.put(element.accessCount, elementsOfCurrentFrequency);
+        }
+
+        element.accessCount++;
+
+        LinkedList<Integer> elementsOfHigherFrequency = this.frequencies.getOrDefault(
+                element.accessCount,
+                new LinkedList<>()
+        );
+        elementsOfHigherFrequency.addFirst(element.key);
+        this.frequencies.put(element.accessCount, elementsOfHigherFrequency);
+    }
+
+    private static class CachedElement<K, V> {
+
+        private final K key;
+
+        private V value;
+
+        private int accessCount;
+
+        CachedElement(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.accessCount = 1;
+        }
     }
 }
 
