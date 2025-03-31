f166dbf5e320a956a614119bcb27bdf8b5c7eea1
itachi <harshkushwaha4151@gmail.com>
2025-03-31 23:30:32 +0530

    DSA commit

--- Step 10  Sliding Window & Two Pointer Combined Problems/Lec 1 Medium Problems/Sliding_Window_Two_Pointer.java
+++ Step 10  Sliding Window & Two Pointer Combined Problems/Lec 1 Medium Problems/Sliding_Window_Two_Pointer.java
@@ -51,3 +51,26 @@ class Solution {
         return end-start;
     }
 }
+
+// Find length of the longest subarray containing atmost two distinct integers
+
+class Solution {
+    public static int totalElements(Integer[] arr) {
+        int l=0,r=0,maxlen=1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(r<arr.length){
+            map.put(arr[r],map.getOrDefault(arr[r],0)+1);
+            if(map.size()>2){
+                map.put(arr[l],map.get(arr[l])-1);
+                if(map.get(arr[l])==0)map.remove(arr[l]);
+                l++;
+            }
+            if(map.size()<=2){
+                int len=r-l+1;
+                maxlen=Math.max(len,maxlen);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+}
