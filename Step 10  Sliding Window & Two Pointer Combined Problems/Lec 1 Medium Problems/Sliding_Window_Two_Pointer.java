087e7c90f6ccaa0f23d9763831b12f2a7e4eac5c
itachi <harshkushwaha4151@gmail.com>
2025-04-01 16:18:11 +0530

    DSA commit

--- Step 10  Sliding Window & Two Pointer Combined Problems/Lec 1 Medium Problems/Sliding_Window_Two_Pointer.java
+++ Step 10  Sliding Window & Two Pointer Combined Problems/Lec 1 Medium Problems/Sliding_Window_Two_Pointer.java
@@ -165,3 +165,30 @@ class Solution {
     }
 
 }
+
+// Maximum Points You Can Obtain from Cards
+
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+      
+        int lSum = 0;
+        int rSum = 0;
+        int maxSum =0;
+
+        for(int i=0; i<k; i++){
+            lSum += cardPoints[i];
+        }
+        maxSum = lSum;
+
+        int idx = cardPoints.length-1;
+        for(int i=k-1; i>=0; i--){
+            lSum -= cardPoints[i];
+            rSum += cardPoints[idx];
+            idx--;
+
+            maxSum = Math.max(maxSum,lSum+rSum);
+        }
+
+        return maxSum;
+    }
+}
