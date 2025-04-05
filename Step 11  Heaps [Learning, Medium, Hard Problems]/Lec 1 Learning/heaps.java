f8c528c48eae748e4d6f0905f47abbb21c5e9b3f
itachi <harshkushwaha4151@gmail.com>
2025-04-05 23:11:24 +0530

    DSA commit

--- Step 11  Heaps [Learning, Medium, Hard Problems]/Lec 1 Learning/heaps.java
+++ Step 11  Heaps [Learning, Medium, Hard Problems]/Lec 1 Learning/heaps.java
@@ -77,3 +77,17 @@ class MinHeap {
         }
     }
 }
+
+// Does array represent Heap
+
+class Solution {
+    public boolean countSub(long arr[], long n)
+    {
+        for(int i=0;i<n;i++){
+            int left=2*i+1;
+            int right=2*i+2;
+            if(left<n && arr[left]>arr[i] || right<n&&arr[right]>arr[i])return false;
+        }
+        return true;
+    }
+}
