0883103ed4d86d7f3531039bae414f5057f8a9c7
itachi <harshkushwaha4151@gmail.com>
2025-04-10 19:55:55 +0530

    DSA commit

--- Step 11  Heaps [Learning, Medium, Hard Problems]/Lec 3 Hard Problems/heaps.java
+++ Step 11  Heaps [Learning, Medium, Hard Problems]/Lec 3 Hard Problems/heaps.java
@@ -123,3 +123,48 @@ class Solution {
         return prev[n];
     }
 } 
+
+// Kth Largest Element in a Stream
+
+class KthLargest {
+    private PriorityQueue<Integer> heap = new PriorityQueue<>();
+    private int k;
+  
+    public KthLargest(int k, int[] nums) {
+      this.k = k;
+      for (var n : nums) add(n);
+    }
+    
+    public int add(int val) {
+      heap.offer(val);
+      if (heap.size() > k) heap.poll();
+      return heap.peek();
+    }
+  }
+
+// Find Median from Data Stream
+
+class MedianFinder {
+    PriorityQueue<Integer> max;
+    PriorityQueue<Integer> min;
+
+    public MedianFinder() {
+        max = new PriorityQueue<>(Collections.reverseOrder());
+        min = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        max.add(num);
+        min.add(max.poll());
+        if (min.size() > max.size()) {
+            max.add(min.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (max.size() > min.size()) {
+            return max.peek();
+        }
+        return (max.peek() + min.peek())/2.0;
+    }
+}
