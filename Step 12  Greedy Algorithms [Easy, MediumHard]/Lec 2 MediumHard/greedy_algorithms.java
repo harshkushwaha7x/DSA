e9d1042d27d8b9fd1e0850b94d6d0fee1114aa8b
itachi <harshkushwaha4151@gmail.com>
2025-04-18 00:28:21 +0530

    DSA commit

--- Step 12  Greedy Algorithms [Easy, MediumHard]/Lec 2 MediumHard/greedy_algorithms.java
+++ Step 12  Greedy Algorithms [Easy, MediumHard]/Lec 2 MediumHard/greedy_algorithms.java
@@ -26,3 +26,17 @@ class Solution {
         return cnt;
     }
 }
+
+// Jump Game
+
+class Solution {
+    public boolean canJump(int[] nums) {
+       int reachable = 0;
+       for(int i = 0; i < nums.length; i ++) {
+           if(i > reachable) return false;
+           reachable = Math.max(reachable, i + nums[i]);
+       } 
+       return true;
+    }
+}
+
