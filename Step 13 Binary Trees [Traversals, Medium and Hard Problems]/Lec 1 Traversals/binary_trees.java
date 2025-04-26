b1fcada3f4e4da3aad8495e7706d2b5170c4197b
itachi <harshkushwaha4151@gmail.com>
2025-04-27 00:07:20 +0530

    DSA commit

--- Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 1 Traversals/binary_trees.java
+++ Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 1 Traversals/binary_trees.java
@@ -19,3 +19,19 @@ class Solution {
     return Traversal(root,arr);
     }
 }
+
+// Binary Tree Inorder Traversal
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        list.addAll(inorderTraversal(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal(root.right));
+        return list;
+    }
+}
+
