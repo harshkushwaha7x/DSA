74490b56398c01e940e0bc2d8b637e75ce1e4825
itachi <harshkushwaha4151@gmail.com>
2025-04-28 19:36:41 +0530

    DSA commit

--- Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 1 Traversals/binary_trees.java
+++ Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 1 Traversals/binary_trees.java
@@ -35,3 +35,20 @@ class Solution {
     }
 }
 
+// Binary Tree Postorder Traversal
+
+class Solution {
+    private void find(TreeNode root, List<Integer> ans) {
+        if (root == null) return;
+        find(root.left, ans);
+        find(root.right, ans);
+        ans.add(root.val);
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        find(root, ans);
+        return ans;
+    }
+}
+
