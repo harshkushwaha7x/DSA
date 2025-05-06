3d72141567e648b110fe360876e84fabe9146c5f
itachi <harshkushwaha4151@gmail.com>
2025-05-07 02:14:43 +0530

    DSA commit

--- Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 2 Medium Problems/binary_trees.java
+++ Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 2 Medium Problems/binary_trees.java
@@ -56,3 +56,27 @@ class Solution {
     }
 }
 
+// Binary Tree Maximum Path Sum
+
+class Solution {
+    private int res = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxsum(root);
+        return res;
+    }
+
+    private int maxsum(TreeNode root) {
+        if (root == null) return 0;
+
+       int ls=maxsum(root.left);
+       int rs=maxsum(root.right);
+
+        int temp = Math.max(Math.max(ls,rs)+root.val,root.val);
+        int max = Math.max(temp, ls + rs + root.val);
+
+        res = Math.max(res, max);
+        return temp;
+    }
+}
+
