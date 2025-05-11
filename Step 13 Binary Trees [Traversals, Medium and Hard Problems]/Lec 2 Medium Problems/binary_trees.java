32cfa01675b77e776e478d9a857fefaacacf97d0
itachi <harshkushwaha4151@gmail.com>
2025-05-11 23:31:43 +0530

    DSA commit

--- Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 2 Medium Problems/binary_trees.java
+++ Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 2 Medium Problems/binary_trees.java
@@ -143,3 +143,32 @@ class Solution {
         dfs(node.right,r+1,c+1,tm);
     }
 }
+
+// Binary Tree Right Side View
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()){
+            TreeNode right = null;
+            int n = queue.size();
+            for (int i = 0; i < n; i++){
+                TreeNode curr = queue.poll();
+                if (curr != null){
+                    right = curr;
+                    queue.offer(curr.left);
+                    queue.offer(curr.right);
+                }
+            }
+            if (right != null){
+                list.add(right.val);
+            }
+        }
+        return list;
+    }
+}
+
