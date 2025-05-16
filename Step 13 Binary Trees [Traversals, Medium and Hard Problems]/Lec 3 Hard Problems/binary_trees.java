0f336ecac90261e7c6d6d6a0e2e6cae21d66021d
itachi <harshkushwaha4151@gmail.com>
2025-05-16 20:46:22 +0530

    DSA commit

--- Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 3 Hard Problems/binary_trees.java
+++ Step 13 Binary Trees [Traversals, Medium and Hard Problems]/Lec 3 Hard Problems/binary_trees.java
@@ -116,3 +116,25 @@ class Solution {
     }
 }
 
+// Construct Binary Tree from Preorder and Inorder Traversal
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<inorder.length;i++)
+           map.put(inorder[i],i);
+        int preindex[] = {0};
+        return build(preorder,map,preindex,0,inorder.length-1);
+    }
+    public TreeNode build(int preorder[], HashMap<Integer,Integer> map, int preindex[], int left, int right){
+        if(left > right)
+         return null;
+         int rootVal = preorder[preindex[0]];
+         preindex[0]++;
+         TreeNode root = new TreeNode(rootVal);
+         int index = map.get(rootVal);
+         root.left = build(preorder,map,preindex,left,index-1);
+         root.right = build(preorder,map,preindex,index+1,right);
+         return root;
+    }
+}
