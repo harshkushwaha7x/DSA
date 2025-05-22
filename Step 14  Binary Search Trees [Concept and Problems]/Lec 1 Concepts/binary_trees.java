316acb67443070b8d90658afb2438ed5498895e7
itachi <harshkushwaha4151@gmail.com>
2025-05-22 23:25:34 +0530

    DSA commit

---
+++ Step 14  Binary Search Trees [Concept and Problems]/Lec 1 Concepts/binary_trees.java
@@ -0,0 +1,12 @@
+// Search in a Binary Search Tree
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root!=null && root.val!=val)
+        {
+           root= val<root.val?root.left:root.right;
+        }
+        return root;
+        
+    }
+}
