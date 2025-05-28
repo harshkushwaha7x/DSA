78419632387d8f21929e93375bd1e467a500b7d1
itachi <harshkushwaha4151@gmail.com>
2025-05-28 23:34:25 +0530

    DSA commit

--- Step 14  Binary Search Trees [Concept and Problems]/Lec 2 Practice Problems/binary_trees.java
+++ Step 14  Binary Search Trees [Concept and Problems]/Lec 2 Practice Problems/binary_trees.java
@@ -67,3 +67,27 @@ class Solution {
         inorderTraversal(root.right, k);
     }
 }
+
+// Validate Binary Search Tree
+
+class Solution {
+    static boolean flag = true;
+    static TreeNode prev = null;
+    public void inorder(TreeNode root) {
+        if(root==null) return;
+        inorder(root.left);
+        if(prev==null) prev = root;
+        else if(root.val<=prev.val){
+            flag = false;
+        }
+        else prev = root;
+        inorder(root.right);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        flag = true;
+        prev = null;
+        inorder(root);
+        return flag;
+    }
+}
