afd84f344183e5a580fa71e591568f1b4e03a895
itachi <harshkushwaha4151@gmail.com>
2025-05-29 22:37:50 +0530

    DSA commit

--- Step 14  Binary Search Trees [Concept and Problems]/Lec 2 Practice Problems/binary_trees.java
+++ Step 14  Binary Search Trees [Concept and Problems]/Lec 2 Practice Problems/binary_trees.java
@@ -91,3 +91,20 @@ class Solution {
         return flag;
     }
 }
+
+// Lowest Common Ancestor of a Binary Search Tree
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        int curr = root.val;
+        
+        if (curr < p.val && curr < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (curr > p.val && curr > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
