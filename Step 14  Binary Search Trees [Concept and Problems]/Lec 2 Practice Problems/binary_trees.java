896ff38f04ad78f40cc535b7a75d54afe176fdb3
itachi <harshkushwaha4151@gmail.com>
2025-06-01 16:54:44 +0530

    DSA commit

--- Step 14  Binary Search Trees [Concept and Problems]/Lec 2 Practice Problems/binary_trees.java
+++ Step 14  Binary Search Trees [Concept and Problems]/Lec 2 Practice Problems/binary_trees.java
@@ -162,3 +162,74 @@ class BSTIterator {
         }
     }
 }
+
+// Two Sum IV - Input is a BST
+
+class Solution{
+    public boolean findTarget(TreeNode root, int k){
+        HashSet<Integer> set=new HashSet<>();
+        return dfs(root,set,k);
+    }
+    private boolean dfs(TreeNode node, HashSet<Integer> set, int k){
+        if(node==null){
+            return false;
+        }
+        if(set.contains(k-node.val)){
+            return true;
+        }
+        set.add(node.val);
+        return dfs(node.left, set, k) || dfs(node.right, set, k);
+    }
+}
+
+// Recover Binary Search Tree
+
+class Solution {
+    public void recoverTree(TreeNode root) {
+        TreeNode first = null;
+        TreeNode second = null;
+        TreeNode prev = null;
+
+        TreeNode current = root;
+        while(current!=null){
+            if(current.left == null){
+                if(prev!=null && prev.val>current.val){
+                    if(first == null){
+                        first = prev;
+                        second = current;
+                    }else{
+                        second = current;
+                    }
+                }
+                prev=current;
+                current = current.right;
+            }else{
+                TreeNode predecessor = current.left;
+                while(predecessor.right!=null && predecessor.right!=current){
+                    predecessor = predecessor.right;
+                }
+                if(predecessor.right == null){
+                    predecessor.right = current;
+                    current = current.left;
+                }else{
+                    predecessor.right = null;
+                    if(prev != null && prev.val>current.val){
+                        if(first == null){
+                            first = prev;
+                            second = current;
+                        }else{
+                            second = current;
+                        }
+                    }
+                    prev = current;
+                    current = current.right;
+                }
+            }
+        }
+        if(first!=null && second!=null){
+            int temp = first.val;
+            first.val = second.val;
+            second.val = temp;
+        }
+    }
+}
