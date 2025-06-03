f6a088a4b0f04e1c0d416ec089c0db77c595ea62
itachi <harshkushwaha4151@gmail.com>
2025-06-03 22:11:01 +0530

    DSA commit

--- Step 15  Graphs [Concepts & Problems]/Lec 2 Problems on BFSDFS/graphs.java
+++ Step 15  Graphs [Concepts & Problems]/Lec 2 Problems on BFSDFS/graphs.java
@@ -38,3 +38,62 @@ class Solution {
         }
     }
 }
+
+// Rotting Oranges
+
+class Solution {
+    int max = 0;
+    int row;
+    int col;
+    int[][] DIRECTIONS = new int[][] { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
+    
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> queue = new LinkedList<>();
+        this.row = grid.length;
+        this.col = grid[0].length;
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (grid[r][c] == 2) {
+                    queue.offer(new int[] {r, c, 0});
+                }
+            }
+        }
+
+        bfs(grid, queue);
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (grid[r][c] == 1) {
+                    return -1;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public void bfs(int[][] grid, Queue<int[]> queue) {
+        while (!queue.isEmpty()) {
+            int[] entry = queue.poll();
+            int r = entry[0];
+            int c = entry[1];
+            int time = entry[2];
+
+            max = Math.max(max, time);
+
+            for (int[] dir : DIRECTIONS) {
+                int new_r = r + dir[0];
+                int new_c = c + dir[1];
+
+                if (new_r >= 0 && new_r < row && new_c >= 0 && new_c < col && grid[new_r][new_c] == 1) {
+                    grid[new_r][new_c] = 2;
+                    queue.offer(new int[] {new_r, new_c, time + 1});
+                }
+            }
+            
+        }
+
+        return;
+    }
+}
