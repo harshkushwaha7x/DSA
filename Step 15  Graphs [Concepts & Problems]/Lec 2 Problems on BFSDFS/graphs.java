adaf107dc3ae0f5c62019b1dc07f6b40be7ac889
itachi <harshkushwaha4151@gmail.com>
2025-06-05 22:55:03 +0530

    DSA commit

--- Step 15  Graphs [Concepts & Problems]/Lec 2 Problems on BFSDFS/graphs.java
+++ Step 15  Graphs [Concepts & Problems]/Lec 2 Problems on BFSDFS/graphs.java
@@ -129,3 +129,83 @@ class Solution {
         }
     }
 }
+
+// 01 Matrix
+
+class Solution {
+    int m, n;
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    public int[][] updateMatrix(int[][] matrix) {
+        m = matrix.length;
+        n = matrix[0].length;
+        
+        int[][] result = new int[m][n];
+        
+        Queue<int[]> que = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    result[i][j] = 0;
+                    que.offer(new int[] {i, j});
+                }
+                else {
+                    result[i][j] = -1;
+                }
+            }
+        }
+
+        while (!que.isEmpty()) {
+            int[] cell = que.poll();
+            int i = cell[0];
+            int j = cell[1];
+            
+            for (int[] dir : directions) {
+                
+                int new_i = i + dir[0];
+                int new_j = j + dir[1];
+                
+                if(new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && result[new_i][new_j] == -1) {
+                    result[new_i][new_j] = result[i][j] + 1;
+                    que.add(new int[] {new_i, new_j});
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
+// Surrounded Regions
+
+class Solution {
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        for(int r = 0 ; r<n ; r++){
+            if(board[r][0]=='O') cross_connection(board, r , 0, n, m);
+            if(board[r][m-1]=='O') cross_connection(board, r, m-1, n, m);
+        }
+
+        for(int c =0 ; c<m ; c++){
+            if(board[0][c]=='O') cross_connection(board, 0 , c, n , m);
+            if(board[n-1][c]=='O') cross_connection(board, n-1, c , n , m);
+        }
+
+        for(int i = 0;i<n; i++){
+            for(int j =0; j<m ; j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='T') board[i][j]='O';
+            }
+        }
+    }
+    public void cross_connection(char[][]board, int i , int j, int n , int m){
+        if(i<0 || i>=n || j<0 || j>=m || board[i][j]!='O') return;
+
+        board[i][j] = 'T';
+        cross_connection(board, i-1, j, n , m);
+        cross_connection(board, i+1, j, n , m);
+        cross_connection(board, i, j-1, n , m);
+        cross_connection(board, i, j+1, n , m);
+    }
+}
