635c0ca47fd43335beeb5e13ef51b70c41f058fe
itachi <harshkushwaha4151@gmail.com>
2025-06-13 20:48:25 +0530

    DSA commit

--- Step 15  Graphs [Concepts & Problems]/Lec 4 Shortest Path Algorithms and Problems/graphs.java
+++ Step 15  Graphs [Concepts & Problems]/Lec 4 Shortest Path Algorithms and Problems/graphs.java
@@ -29,3 +29,58 @@ class Solution {
         return -1;
     }
 }
+
+// Path With Minimum Effort
+
+class Solution {
+    static class Edge {
+        int effort, row, col;
+        Edge(int e, int r, int c) {
+            this.effort = e;
+            this.row = r;
+            this.col = c;
+        }
+    }
+
+    public int minimumEffortPath(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+        int[][] dist = new int[n][m];
+        for (int[] row : dist) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        dist[0][0] = 0;
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.effort - b.effort);
+        pq.offer(new Edge(0, 0, 0));
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int e = edge.effort;
+            int r = edge.row;
+            int c = edge.col;
+
+            if (r == n - 1 && c == m - 1) return e;
+
+            for (int i = 0; i < 4; i++) {
+                int nRow = r + dx[i];
+                int nCol = c + dy[i];
+
+                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m) {
+                    int currentEffort = Math.abs(heights[r][c] - heights[nRow][nCol]);
+                    int maxEffort = Math.max(e, currentEffort);
+
+                    if (dist[nRow][nCol] > maxEffort) {
+                        dist[nRow][nCol] = maxEffort;
+                        pq.offer(new Edge(maxEffort, nRow, nCol));
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
