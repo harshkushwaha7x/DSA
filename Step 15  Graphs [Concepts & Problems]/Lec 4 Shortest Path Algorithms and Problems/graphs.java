a411e23498296cba030f3788f20359736c8b8247
itachi <harshkushwaha4151@gmail.com>
2025-06-16 21:18:40 +0530

    DSA commit

--- Step 15  Graphs [Concepts & Problems]/Lec 4 Shortest Path Algorithms and Problems/graphs.java
+++ Step 15  Graphs [Concepts & Problems]/Lec 4 Shortest Path Algorithms and Problems/graphs.java
@@ -225,3 +225,40 @@ class Solution {
     return (int) ways[dst];
   }
 }
+
+// Find the City With the Smallest Number of Neighbors at a Threshold Distance
+
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int ans=n;
+        int c=n;
+            int[][] matrix = new int[n][n];
+            for(int i=0;i<n;i++){
+                Arrays.fill(matrix[i],(int)1e9);
+                matrix[i][i]=0;
+            }
+            for(int edge[] : edges){
+                matrix[edge[0]][edge[1]] = edge[2];
+                matrix[edge[1]][edge[0]] = edge[2];
+            }
+            for(int src=0;src<n;src++){
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][src] + matrix[src][j]);
+                    }
+                }
+            }
+            for(int src=0;src<n;src++){
+                int temp=0;
+                for(int i=0;i<n;i++){
+                    if(src==i) continue;
+                    if(matrix[src][i] <= distanceThreshold) temp++;
+                }
+                if(temp<=c){
+                    c=temp;
+                    ans=src;
+                }
+            }
+    return ans;
+    }
+}
