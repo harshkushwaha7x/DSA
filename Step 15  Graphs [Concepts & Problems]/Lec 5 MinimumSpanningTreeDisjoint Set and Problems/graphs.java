bb053ebc60d227af346af0cbd588481350579a98
itachi <harshkushwaha4151@gmail.com>
2025-06-23 22:54:24 +0530

    DSA commit

--- Step 15  Graphs [Concepts & Problems]/Lec 5 MinimumSpanningTreeDisjoint Set and Problems/graphs.java
+++ Step 15  Graphs [Concepts & Problems]/Lec 5 MinimumSpanningTreeDisjoint Set and Problems/graphs.java
@@ -69,3 +69,84 @@ class Solution {
         return parent[ind] = find_parent(parent, parent[ind]);
     }
 }
+
+// Accounts Merge
+
+class Solution {
+    static class DSU {
+        int[] parent;
+        int[] size;
+        public DSU(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for(int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int findParent(int x) {
+            if(x == parent[x]) return x;
+            return parent[x] = findParent(parent[x]);
+        }
+
+        public void union(int u, int v) {
+            int pu = findParent(u);
+            int pv = findParent(v);
+            if(pu == pv) return;
+            if(size[pu] < size[pv]) {
+                parent[pu] = pv;
+                size[pv] += size[pu];
+            } else {
+                parent[pv] = pu;
+                size[pu] += size[pv];
+            }
+        }
+    }
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        int n = accounts.size();
+        DSU dsu = new DSU(n);
+        Map<String, Integer> map = new HashMap<>();
+
+        // find the owner of each mail and store
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j < accounts.get(i).size(); j++) {
+                String email = accounts.get(i).get(j);
+                if(!map.containsKey(email)) {
+                    map.put(email,i);
+                } else {
+                    dsu.union(i, map.get(email));
+                }
+            }
+        }
+
+        List<String>[] mails = new ArrayList[n];
+
+        for(int i = 0; i < n; i++) {
+            mails[i] = new ArrayList<>();
+        }
+
+        // based on parents, group mails
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            String key = entry.getKey();
+            int index = dsu.findParent(entry.getValue());
+            mails[index].add(key);
+        }
+
+        List<List<String>> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            if(mails[i].size() == 0) 
+                continue;
+            Collections.sort(mails[i]);
+            List<String> ls = new ArrayList<>();
+            ls.add(accounts.get(i).get(0));
+            for(String m : mails[i]) {
+                ls.add(m);
+            }
+            list.add(ls);
+        }
+
+        return list;
+    }
+}
