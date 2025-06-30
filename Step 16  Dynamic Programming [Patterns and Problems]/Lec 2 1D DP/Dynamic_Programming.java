8e130c2ef402fd137432efb1536061b63f4e426e
itachi <harshkushwaha4151@gmail.com>
2025-07-01 00:12:51 +0530

    DSA commit

--- Step 16  Dynamic Programming [Patterns and Problems]/Lec 2 1D DP/Dynamic_Programming.java
+++ Step 16  Dynamic Programming [Patterns and Problems]/Lec 2 1D DP/Dynamic_Programming.java
@@ -29,3 +29,34 @@ class Solution {
         return Math.max(rob, norob);
     }
 }
+
+// House Robber II
+
+class Solution {
+    int solve(int[] nums, int n, int[] dp) {
+        if (n >= nums.length)
+            return 0;
+        if (dp[n] != -1)
+            return dp[n];
+        int include = nums[n] + solve(nums, n + 2, dp);
+        int exclude = solve(nums, n + 1, dp);
+        dp[n] = Math.max(include, exclude);
+        return dp[n];
+    }
+
+    public int rob(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+
+        int arr[]= new int[nums.length-1];
+        for(int i=0;i<nums.length-1;i++)
+        arr[i] =nums[i];
+
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp, -1);
+        int dp2[] = new int[nums.length];
+        Arrays.fill(dp2, -1);
+
+        return Math.max(solve(nums, 1, dp), solve(arr,0, dp2));
+    }
+}
