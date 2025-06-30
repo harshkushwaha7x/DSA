95fc370ea901da2fa65c7eae50362cb26495935f
itachi <harshkushwaha4151@gmail.com>
2025-06-30 23:23:00 +0530

    DSA commit

--- Step 16  Dynamic Programming [Patterns and Problems]/Lec 2 1D DP/Dynamic_Programming.java
+++ Step 16  Dynamic Programming [Patterns and Problems]/Lec 2 1D DP/Dynamic_Programming.java
@@ -13,3 +13,19 @@ class Solution {
         
     }    
 }
+
+// House Robber
+
+class Solution {
+    public int rob(int[] nums) {
+        int rob = 0;
+        int norob = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            int newRob = norob + nums[i];
+            int newNoRob = Math.max(norob, rob);
+            rob = newRob;
+            norob = newNoRob;
+        }
+        return Math.max(rob, norob);
+    }
+}
