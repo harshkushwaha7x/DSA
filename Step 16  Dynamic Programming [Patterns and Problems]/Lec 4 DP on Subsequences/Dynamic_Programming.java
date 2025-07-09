aa4877668e9b7f99a6f52fb76334cf871deb463c
itachi <harshkushwaha4151@gmail.com>
2025-07-09 23:36:19 +0530

    DSA commit

--- Step 16  Dynamic Programming [Patterns and Problems]/Lec 4 DP on Subsequences/Dynamic_Programming.java
+++ Step 16  Dynamic Programming [Patterns and Problems]/Lec 4 DP on Subsequences/Dynamic_Programming.java
@@ -19,3 +19,34 @@ class Solution {
         return dp[sum];
     }
 }
+
+// Partition Array Into Two Arrays to Minimize Sum Difference
+
+class Solution {
+
+    int ans=Integer.MAX_VALUE;
+    public int minimumDifference(int[] nums) {
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        call(nums,0,0,sum,0);
+       return ans;
+    }
+public void call(int []nums,int sum,int i,int totalSum,int count){
+    int n=nums.length/2;
+    
+    if( count==n ){
+        int x=Math.abs(totalSum-(2*sum));
+        ans=Math.min(ans,x);
+         return;
+    }
+     if(i>=nums.length ){
+        return;
+     }
+   
+   call(nums,sum+nums[i],i+1,totalSum,count+1);
+   call(nums,sum,i+1,totalSum,count);
+    
+}
+}
