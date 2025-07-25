1b9b9b5d077f1304979cceb689047e91ab7e2a4e
itachi <harshkushwaha4151@gmail.com>
2025-07-25 22:31:37 +0530

    DSA commit

--- Step 16  Dynamic Programming [Patterns and Problems]/Lec 6 DP on Stocks/Dynamic_Programming.java
+++ Step 16  Dynamic Programming [Patterns and Problems]/Lec 6 DP on Stocks/Dynamic_Programming.java
@@ -48,3 +48,26 @@ class Solution {
         return sell2;
     }
 }
+
+// Best Time to Buy and Sell Stock IV
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n][k+1][2];
+        for(int i=0; i<=k; i++){
+            dp[0][i][1] -= prices[0];
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=k; j++){
+                dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i]);
+            }
+        }
+        int ans = 0;
+        for(int i=0; i<=k; i++){
+            ans = Math.max(ans,dp[n-1][i][0]);
+        }
+        return ans;
+    }
+}
