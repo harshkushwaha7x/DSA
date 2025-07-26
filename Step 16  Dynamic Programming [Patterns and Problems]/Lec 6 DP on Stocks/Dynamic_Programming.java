75752dce3d743f8cafe384e164f3aa2ed5c50194
itachi <harshkushwaha4151@gmail.com>
2025-07-27 01:12:49 +0530

    DSA commit

--- Step 16  Dynamic Programming [Patterns and Problems]/Lec 6 DP on Stocks/Dynamic_Programming.java
+++ Step 16  Dynamic Programming [Patterns and Problems]/Lec 6 DP on Stocks/Dynamic_Programming.java
@@ -91,3 +91,19 @@ class Solution {
         return sell[length];
     }
 }
+
+// Best Time to Buy and Sell Stock with Transaction Fee
+
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        int hold = -prices[0]; 
+        int cash = 0;           
+        for (int i = 1; i < n; i++) {
+            hold = Math.max(hold, cash - prices[i]);
+            cash = Math.max(cash, hold + prices[i] - fee);
+        }
+        return cash; 
+    }
+}
