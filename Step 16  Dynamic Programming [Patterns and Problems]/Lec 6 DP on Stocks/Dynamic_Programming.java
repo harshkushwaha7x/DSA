e095c98d5096010b55589d491cd0c8f995dfb2f3
itachi <harshkushwaha4151@gmail.com>
2025-07-26 22:47:10 +0530

    DSA commit

--- Step 16  Dynamic Programming [Patterns and Problems]/Lec 6 DP on Stocks/Dynamic_Programming.java
+++ Step 16  Dynamic Programming [Patterns and Problems]/Lec 6 DP on Stocks/Dynamic_Programming.java
@@ -71,3 +71,23 @@ class Solution {
         return ans;
     }
 }
+
+// Best Time to Buy and Sell Stock with Cooldown
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 1) {
+            return 0;
+        }
+        int length = prices.length;
+        int[] buy = new int[length + 1];
+        int[] sell = new int[length + 1];
+        buy[1] = -prices[0];
+        for (int i = 2; i <= length; i++) {
+            int price = prices[i - 1];
+            buy[i] = Math.max(buy[i - 1], sell[i - 2] - price);
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + price);
+        }
+        return sell[length];
+    }
+}
