36193e44f7514ab3d6f832fbb0f00c5954cf7f19
itachi <harshkushwaha4151@gmail.com>
2025-08-06 00:06:10 +0530

    DSA commit

--- Step 16  Dynamic Programming [Patterns and Problems]/Lec 9 DP on Squares/Dynamic_Programming.java
+++ Step 16  Dynamic Programming [Patterns and Problems]/Lec 9 DP on Squares/Dynamic_Programming.java
@@ -62,3 +62,20 @@ class Solution {
         return maxRectangle;
     }
 }
+
+// Count Square Submatrices with All Ones
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        int squares = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1)
+                    dp[i + 1][j + 1] = 1 + Math.min(dp[i][j], Math.min(dp[i + 1][j], dp[i][j + 1]));
+                squares += dp[i + 1][j + 1];
+            }
+        }
+        return squares;
+    }
+}
