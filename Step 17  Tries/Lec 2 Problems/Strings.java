a0fa208705226e901895ec238be83a3ad9249740
itachi <harshkushwaha4151@gmail.com>
2025-08-10 01:15:07 +0530

    DSA commit

---
+++ Step 17  Tries/Lec 2 Problems/Strings.java
@@ -0,0 +1,21 @@
+// Minimum Add to Make Parentheses Valid
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int n = s.length();
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i) == '('){
+                stack.push('(');
+            }
+            else{
+                if(!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                }
+                else{
+                    stack.push(')');
+                }
+            }
+        }
+        return stack.size();
+    }
+}
