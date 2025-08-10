ec4bd2349b615836c60b3b1593af2d5edc34b902
itachi <harshkushwaha4151@gmail.com>
2025-08-11 00:03:06 +0530

    DSA commit

--- Step 18  Strings/Lec 1 Hard Problems/Strings.java
+++ Step 18  Strings/Lec 1 Hard Problems/Strings.java
@@ -19,3 +19,29 @@ class Solution {
         return stack.size();
     }
 }
+
+// Count and Say
+class Solution {
+    public String count(int n, String a) {
+        if(n == 1) return a;
+        Map<Character, Integer> map = new HashMap<>();
+        char prev = a.charAt(0);
+        StringBuilder sb = new StringBuilder();
+        int point = 1, count = 1;
+        while(point < a.length()) {
+            if(prev != a.charAt(point)) {
+                sb.append(count).append(prev);
+                count = 1;
+            }
+            else count++;
+            prev = a.charAt(point);
+            point++;
+        }
+        sb.append(count).append(prev);
+        return count(n - 1, sb.toString());
+
+    }
+    public String countAndSay(int n) {
+        return count(n, "1");
+    }
+}
