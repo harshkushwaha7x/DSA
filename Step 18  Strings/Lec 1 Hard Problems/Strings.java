15b85b700b7216eba2a56d360b24535e0dea81ac
itachi <harshkushwaha4151@gmail.com>
2025-08-14 00:24:18 +0530

    DSA commit

--- Step 18  Strings/Lec 1 Hard Problems/Strings.java
+++ Step 18  Strings/Lec 1 Hard Problems/Strings.java
@@ -126,3 +126,10 @@ class Solution {
         
     }
 }
+
+//  Find the Index of the First Occurrence in a String
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
