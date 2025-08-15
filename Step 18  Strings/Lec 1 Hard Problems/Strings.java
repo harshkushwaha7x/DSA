10d274507aa312be675a82cc9f7b756cb3e59940
itachi <harshkushwaha4151@gmail.com>
2025-08-15 23:28:28 +0530

    DSA commit

--- Step 18  Strings/Lec 1 Hard Problems/Strings.java
+++ Step 18  Strings/Lec 1 Hard Problems/Strings.java
@@ -133,3 +133,55 @@ class Solution {
         return haystack.indexOf(needle);
     }
 }
+
+// Shortest Palindrome
+class Solution {
+    long base = 29;
+    long mod = (long)1e9 + 7;
+    public String shortestPalindrome(String s) {
+      long fh = 0;
+      long rh = 0; 
+      long power = 1; 
+      int index = -1; 
+      for(int i =0; i<s.length(); i++)
+      {
+        char ch = s.charAt(i);
+        fh = (fh*base + (ch-'a' + 1))%mod;
+        rh = (rh + (ch-'a'+1)*power)%mod;
+        power = (power*base)%mod;
+        if(fh==rh)
+        index = i; 
+      }
+    String remSuffix = s.substring(index+1);
+    StringBuilder sb = new StringBuilder(remSuffix).reverse();
+    return sb.append(s).toString();
+      
+
+    }
+}
+
+// Longest Happy Prefix
+class Solution {
+    public String longestPrefix(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+        int len = 0;
+        int i = 1;
+
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return s.substring(0, lps[n - 1]);
+    }
+}
